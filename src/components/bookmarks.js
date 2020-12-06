71e0245a1945ae0cba8626ca003867996b4292f5
Bartek <bartek.bober.koder@gmail.com>
2020-12-06 20:54:14 +0100

    add links opened and some refactor

--- src/components/bookmarks.js
+++ src/components/bookmarks.js
@@ -2,32 +2,38 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { selectBookmark, removeBookmarkLink } from '../store/bookmarks'
 import { selectEdit } from '../store/menu'
+import { incrementOpenedLinks } from '../store/stats'
 import ReactTooltip from 'react-tooltip';
 import './styles/Bookmarks.css'
 
 export default  function Bookmarks() {
-  const dispach = useDispatch()
+  const dispatch = useDispatch()
   const bokmarkLinks = useSelector(selectBookmark)
   const isEditing = useSelector(selectEdit)
 
   const prepareLinkToShow = link => link.length > 50 ? `${link.slice(0, 45)}...` : link
-  const deleteLink = id => {
-    dispach(removeBookmarkLink(id))
+  const deleteLink = (id, e) => {
+    e.stopPropagation()
+    dispatch(removeBookmarkLink(id))
+  }
+  const openLink = link => {
+    window.open(link)
+    dispatch(incrementOpenedLinks())
   }
 
   return (
     <div className="bookmark-list-wrapper">
       <ul className="bookmark-list">
-        {bokmarkLinks.map( ({ link, linkAlias, id}) => <li key={id} className="bookmark-link">
+        {bokmarkLinks.map( ({ link, linkAlias, id}) => <li key={id} className="bookmark-link" onClick={() => openLink(link)}>
           <div>
-            <span className="link-alias">{linkAlias}:</span>
+            {linkAlias ? <span className="link-alias">{linkAlias}:</span> : null}
             <a href={link} target="_blank" rel="noreferrer" data-tip={link.length > 50 ? link : null}>
               {prepareLinkToShow(link)}
             </a>
           </div>
           
           {isEditing ?
-            <button className="bookmark-delete" onClick={() =>  deleteLink(id)}>🗑</button> 
+            <button className="bookmark-delete" onClick={e =>  deleteLink(id, e)}>🗑</button> 
           : <ReactTooltip />}
         </li>)}
       </ul>
