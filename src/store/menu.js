78482a0874dafa09c60126c4919d2468dc4c6a00
bart <bartek.bober.koder@gmail.com>
2021-06-01 20:17:22 +0200

    add menu optin

--- src/store/menu.js
+++ src/store/menu.js
@@ -1,37 +1,48 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
+import { getStorage, setStograge } from "../utils";
+
+const storage = JSON.parse(getStorage());
 
 const initialState = {
   edit: false,
   changePosition: false,
-  dataImporting: false
-}
+  dataImporting: false,
+  newTabOpening: storage?.config ? storage.config.newTabOpening : false,
+};
 
 export const menuSlice = createSlice({
-  name: 'menu',
+  name: "menu",
   initialState,
   reducers: {
-    toggleEdit: state => {
+    toggleEdit: (state) => {
       if (state.changePosition) {
-        state.changePosition = false
+        state.changePosition = false;
       }
-      state.edit = !state.edit
+      state.edit = !state.edit;
     },
-    toggleChangingPosition: state => {
+    toggleChangingPosition: (state) => {
       if (state.edit) {
-        state.edit = false
+        state.edit = false;
       }
-      state.changePosition = !state.changePosition
+      state.changePosition = !state.changePosition;
     },
-    toggleDataImporting: state => {
-      state.dataImporting = !state.dataImporting
-    }
-  }
+    toggleDataImporting: (state) => {
+      state.dataImporting = !state.dataImporting;
+    },
+    toggleNewTabOpening: (state) => {
+      state.newTabOpening = !state.newTabOpening;
+      const storage = JSON.parse(getStorage());
+      storage.config.newTabOpening = state.newTabOpening;
+      setStograge(storage);
+    },
+  },
 });
 
-export const { toggleEdit, toggleChangingPosition, toggleDataImporting } = menuSlice.actions
+export const { toggleEdit, toggleChangingPosition, toggleDataImporting, toggleNewTabOpening } = menuSlice.actions;
 
-export const selectEdit = state => state.menu.edit
-export const selectChangePosition = state => state.menu.changePosition
-export const selectDataImporting = state => state.menu.dataImporting
+export const selectEdit = (state) => state.menu.edit;
+export const selectChangePosition = (state) => state.menu.changePosition;
+export const selectDataImporting = (state) => state.menu.dataImporting;
+export const selectNewTabOpening = (state) => state.menu.newTabOpening;
 
 export default menuSlice.reducer;
--- src/utils.js
+++ src/utils.js
@@ -1,51 +1,68 @@
-const STORAGE_NAME = 'homeScreenApp'
+const STORAGE_NAME = "homeScreenApp";
 const STORAGE_MODEL = {
   links: [],
   bookmark: [],
   stats: {
-    linksOpened: 0
-  }
-}
-const getStorage = () => localStorage.getItem(STORAGE_NAME)
-const setStograge = (storage) => localStorage.setItem(STORAGE_NAME, JSON.stringify(storage))
-const getLinks = () => JSON.parse(localStorage.getItem(STORAGE_NAME)).links
+    linksOpened: 0,
+  },
+  config: {
+    newTabOpening: false,
+  },
+};
+const getStorage = () => localStorage.getItem(STORAGE_NAME);
+const setStograge = (storage) => localStorage.setItem(STORAGE_NAME, JSON.stringify(storage));
+const getLinks = () => JSON.parse(localStorage.getItem(STORAGE_NAME)).links;
 const addNewLink = (link) => {
-  const storage = JSON.parse(getStorage())
-  storage.links.push(link)
-  setStograge(storage)
-}
+  const storage = JSON.parse(getStorage());
+  storage.links.push(link);
+  setStograge(storage);
+};
 const setStorageLinks = (links) => {
-  const storage = JSON.parse(getStorage())
-  storage.links = links
-  localStorage.setItem(STORAGE_NAME, JSON.stringify(storage))
-}
-const getBookmark = () => JSON.parse(localStorage.getItem(STORAGE_NAME)).bookmark
-const setBookmarkLinks = bookmark => {
-  const storage = JSON.parse(getStorage())
-  storage.bookmark = bookmark
-  localStorage.setItem(STORAGE_NAME, JSON.stringify(storage))
-}
+  const storage = JSON.parse(getStorage());
+  storage.links = links;
+  localStorage.setItem(STORAGE_NAME, JSON.stringify(storage));
+};
+const getBookmark = () => JSON.parse(localStorage.getItem(STORAGE_NAME)).bookmark;
+const setBookmarkLinks = (bookmark) => {
+  const storage = JSON.parse(getStorage());
+  storage.bookmark = bookmark;
+  localStorage.setItem(STORAGE_NAME, JSON.stringify(storage));
+};
 const addBookmarkLink = (bookmarkLink) => {
-  const storage = JSON.parse(getStorage())
-  storage.bookmark.push(bookmarkLink)
-  setStograge(storage)
-}
+  const storage = JSON.parse(getStorage());
+  storage.bookmark.push(bookmarkLink);
+  setStograge(storage);
+};
 const increaseOpenedLinks = () => {
-  const storage = JSON.parse(getStorage())
-  storage.stats.linksOpened = storage.stats.linksOpened+1
-  setStograge(storage)
-}
-const getStats = () => JSON.parse(getStorage()).stats
+  const storage = JSON.parse(getStorage());
+  storage.stats.linksOpened = storage.stats.linksOpened + 1;
+  setStograge(storage);
+};
+const getStats = () => JSON.parse(getStorage()).stats;
 
-const uuid =() => {
+const uuid = () => {
   var dt = new Date().getTime();
-  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
-    var r = (dt + Math.random()*16)%16 | 0;
-    dt = Math.floor(dt/16);
+  var uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+    var r = (dt + Math.random() * 16) % 16 | 0;
+    dt = Math.floor(dt / 16);
     // eslint-disable-next-line no-mixed-operators
-    return (c === 'x' ? r :(r&0x3|0x8)).toString(16);
+    return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
   });
   return uuid;
-}
+};
 
-export { STORAGE_NAME, STORAGE_MODEL, getStorage, setStograge, getLinks, addNewLink, uuid, setStorageLinks, getBookmark, addBookmarkLink, setBookmarkLinks, increaseOpenedLinks, getStats }
+export {
+  STORAGE_NAME,
+  STORAGE_MODEL,
+  getStorage,
+  setStograge,
+  getLinks,
+  addNewLink,
+  uuid,
+  setStorageLinks,
+  getBookmark,
+  addBookmarkLink,
+  setBookmarkLinks,
+  increaseOpenedLinks,
+  getStats,
+};
