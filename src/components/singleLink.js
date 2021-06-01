fd5950f66a2c80bd0cd3e61038f719502f151aba
bart <bartek.bober.koder@gmail.com>
2021-06-01 20:21:36 +0200

    add possibility to opening in _self

--- src/components/singleLink.js
+++ src/components/singleLink.js
@@ -1,112 +1,103 @@
-import React, { useRef, useState } from 'react'
-import './styles/singleLink.css'
-import { useSelector, useDispatch } from 'react-redux'
-import Draggable from 'react-draggable'
-import { removeById, updateLinkPosition, toggleFavourites } from '../store/links'
-import { toggleEditingModal } from '../store/addLinkModal'
-import { selectEdit, selectChangePosition } from '../store/menu'
-import { incrementOpenedLinks } from '../store/stats'
-import { drag, updatePosition, isDraggableDisabled } from './utils/dragAndDropUtil'
+import React, { useRef, useState } from "react";
+import "./styles/singleLink.css";
+import { useSelector, useDispatch } from "react-redux";
+import Draggable from "react-draggable";
+import { removeById, updateLinkPosition, toggleFavourites } from "../store/links";
+import { toggleEditingModal } from "../store/addLinkModal";
+import { selectEdit, selectChangePosition, selectNewTabOpening } from "../store/menu";
+import { incrementOpenedLinks } from "../store/stats";
+import { drag, updatePosition, isDraggableDisabled } from "./utils/dragAndDropUtil";
 
 export default function SingleLink(props) {
-  const dispatch = useDispatch()
-  const linkWrapper = useRef()
+  const dispatch = useDispatch();
+  const linkWrapper = useRef();
+  const isNewTabOpening = useSelector(selectNewTabOpening);
   const openLink = (href) => {
-    window.open(href)
-    dispatch(incrementOpenedLinks())
-  }
+    window.open(href, !isNewTabOpening && "_self");
+    dispatch(incrementOpenedLinks());
+  };
   const clikcFunc = () => {
-    if (props.feature) props.feature()
+    if (props.feature) props.feature();
     else {
-      if (!isPositionChanging) openLink(props.href)
+      if (!isPositionChanging) openLink(props.href);
     }
-  }
-  
-  const isEditing = useSelector(selectEdit)
-  const isPositionChanging = useSelector(selectChangePosition)
-  const [isElementDragged, setIsElementDragged] = useState(false)
+  };
+
+  const isEditing = useSelector(selectEdit);
+  const isPositionChanging = useSelector(selectChangePosition);
+  const [isElementDragged, setIsElementDragged] = useState(false);
   const getEditingClass = () => {
-    const isOdd = number => number % 2
+    const isOdd = (number) => number % 2;
     if (isEditing && !props.feature) {
       if (isOdd(props.index) === 0) {
-        return 'editing-1'
+        return "editing-1";
       } else {
-        return 'editing-2'
+        return "editing-2";
       }
     }
-  }
+  };
   const isFavourite = () => {
-    if (props.isFavourite) return 'single-link-fovurite'
-    else return ''
-  }
+    if (props.isFavourite) return "single-link-fovurite";
+    else return "";
+  };
 
   const removeLink = (id, e) => {
-    e.stopPropagation()
-    dispatch(removeById(id))
-  }
+    e.stopPropagation();
+    dispatch(removeById(id));
+  };
 
   const openEditWindow = (e, id) => {
-    e.stopPropagation()
-    dispatch(toggleEditingModal(id))
-  }
+    e.stopPropagation();
+    dispatch(toggleEditingModal(id));
+  };
 
   const toggleFavourite = (e, id) => {
-    e.stopPropagation()
-    dispatch(toggleFavourites(id))
-  }
+    e.stopPropagation();
+    dispatch(toggleFavourites(id));
+  };
 
   const updatePostionDraggedElement = (e) => {
-    setIsElementDragged(false)
-    dispatch(updateLinkPosition(updatePosition(e, props.index, linkWrapper)))
-  } 
+    setIsElementDragged(false);
+    dispatch(updateLinkPosition(updatePosition(e, props.index, linkWrapper)));
+  };
 
   return (
-    <Draggable 
+    <Draggable
       disabled={isDraggableDisabled(isPositionChanging, props)}
       onStart={(e) => drag(e, linkWrapper)}
       onDrag={() => setIsElementDragged(true)}
       onStop={(e) => updatePostionDraggedElement(e)}
-      position={{x: 0, y: 0}}
+      position={{ x: 0, y: 0 }}
     >
-      <div className={'single-link-wrapper editing ' + getEditingClass() } ref={linkWrapper} data-index={props.index}>
-        {isEditing && !props.feature ?
+      <div className={"single-link-wrapper editing " + getEditingClass()} ref={linkWrapper} data-index={props.index}>
+        {isEditing && !props.feature ? (
           <div>
-            <button className="link-btn link-btn--delete" onClick={e => removeLink(props.id, e)} data-tip="remove">
+            <button className="link-btn link-btn--delete" onClick={(e) => removeLink(props.id, e)} data-tip="remove">
               <span>🗑</span>
             </button>
 
-            <button className="link-btn link-btn--edit" onClick={e => openEditWindow(e, props.id)} data-tip="edit">
+            <button className="link-btn link-btn--edit" onClick={(e) => openEditWindow(e, props.id)} data-tip="edit">
               <span>⚙️</span>
             </button>
 
-            <button className="link-btn link-btn--favourite" onClick={e => toggleFavourite(e, props.id)}>
+            <button className="link-btn link-btn--favourite" onClick={(e) => toggleFavourite(e, props.id)}>
               <span>⭐️</span>
             </button>
           </div>
-          : 
-          null
-        }
+        ) : null}
 
-        <div className={'single-link ' + isFavourite() } onClick={ ()=> clikcFunc()} >
-          {isPositionChanging && !isElementDragged && !props.feature ?
-            <span className="position-changing-icon">🤚</span>
-          : 
-            null
-          }
+        <div className={"single-link " + isFavourite()} onClick={() => clikcFunc()}>
+          {isPositionChanging && !isElementDragged && !props.feature ? <span className="position-changing-icon">🤚</span> : null}
 
-          { isPositionChanging && isElementDragged ? 
-            <span className="position-changing-icon position-changing-icon--dragged">🤏</span> : null
-          }
+          {isPositionChanging && isElementDragged ? <span className="position-changing-icon position-changing-icon--dragged">🤏</span> : null}
 
-          <span className="fovourite-sign">
-            {props.isFavourite && !isPositionChanging ? '⭐️' : null}
-          </span>
-          
-          {props.img.length > 4 ?
+          <span className="fovourite-sign">{props.isFavourite && !isPositionChanging ? "⭐️" : null}</span>
+
+          {props.img.length > 4 ? (
             <img className="single-link__img" alt="link" src={props.img} />
-            :
+          ) : (
             <span className="single-link__img--default">{props.name.slice(0, 1)}</span>
-          }
+          )}
         </div>
         <span className="single-link-name">{props.name}</span>
       </div>
